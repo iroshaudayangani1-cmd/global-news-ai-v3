9ae07c97ee8bd5ea25a285790bf469a93ae205b7
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-07-15 22:35:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print(" Global News AI v3")
+    print(" GitHub Actions Test")
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
