0e7c52496b5b61e0be34ffd075bdea0380f276d8
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-07-18 21:09:56 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 # GEMINI
 # ==========================
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL = "gemini-2.5-flash-lite"
+GEMINI_MODEL = "gemini-flash-latest"
 # ==========================
 # BLOGGER
 # ==========================
