7bdf19635dffa003dfc571aef2c60d482bb44196
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-07-18 21:03:52 +0530

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -3,9 +3,5 @@ from google import genai
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Say hello."
-)
-
-print(response.text)
+for model in client.models.list():
+    print(model.name)
