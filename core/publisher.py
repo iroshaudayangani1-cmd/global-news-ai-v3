0a03911330526798bd0ca59e1a1bfee1b93408cc
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-07-18 14:18:42 +0530

    Create publisher.py

---
+++ core/publisher.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+from config.settings import REWRITTEN_JSON
+from core.blogger import publish_post
+
+
+def publish_articles():
+
+    if not os.path.exists(REWRITTEN_JSON):
+        raise FileNotFoundError(
+            f"{REWRITTEN_JSON} not found."
+        )
+
+    with open(REWRITTEN_JSON, "r", encoding="utf-8") as f:
+        articles = json.load(f)
+
+    if not articles:
+        print("No rewritten articles found.")
+        return
+
+    article = articles[0]
+
+    title = article["title"]
+    content = article["article"]
+
+    print(f"Publishing: {title}")
+
+    result = publish_post(title, content)
+
+    print("=================================")
+    print("Published Successfully")
+    print(result["url"])
+    print("=================================")
